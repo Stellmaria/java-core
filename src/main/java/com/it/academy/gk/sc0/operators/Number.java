b850b1e6984048938f0ba53ee1d095837fc200c6
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-09-21 22:30:59 +0200

    feat: add solutions for task SumNumber

--- src/main/java/com/it/academy/gk/sc0/operators/Number.java
+++ src/main/java/com/it/academy/gk/sc0/operators/Number.java
@@ -1,5 +1,7 @@
 package com.it.academy.gk.sc0.operators;
 
+import com.it.academy.gk.sc0.operators.util.Const;
+
 import java.util.Arrays;
 
 /**
@@ -19,5 +21,45 @@ public class Number {
      */
     public int getSum(final int... values) {
         return Arrays.stream(values).sum();
+    }
+
+    /**
+     * <p>RU: Дано двузначное и трехзначное число.
+     * Для каждого выведите сумму.</p>
+     * <p>EN : Given a two-digit and a three-digit number.
+     * For each print the sum and the product of the digits.</p>
+     *
+     * @param value число/number.
+     * @return сумма цифр числа/sum of digits.
+     */
+    public int getSumNumbers(final int value) {
+        int sum = 0;
+        int digit = value;
+
+        while (digit != 0) {
+            sum += (digit % Const.MINIMAL_TWO_DIGITAL_NUMBER);
+            digit /= Const.MINIMAL_TWO_DIGITAL_NUMBER;
+        }
+        return sum;
+    }
+
+    /**
+     * <p>RU: Дано двузначное и трехзначное число.
+     * Для каждого выведите произведение чисел.</p>
+     * <p>EN : Given a two-digit and a three-digit number.
+     * For each print the product of numbers..</p>
+     *
+     * @param value число/number.
+     * @return произведение цифр числа/product of digits.
+     */
+    public int getMultiplicationNumber(final int value) {
+        int sum = 1;
+        int digit = value;
+
+        while (digit != 0) {
+            sum *= (digit % Const.MINIMAL_TWO_DIGITAL_NUMBER);
+            digit /= Const.MINIMAL_TWO_DIGITAL_NUMBER;
+        }
+        return sum;
     }
 }
--- src/test/java/com/it/academy/gk/sc0/operators/NumberTest.java
+++ src/test/java/com/it/academy/gk/sc0/operators/NumberTest.java
@@ -38,4 +38,48 @@ class NumberTest {
 
         assertEquals(expected, actual);
     }
+
+    static @NotNull Stream<Arguments> sumNumbersProviderArguments() {
+        return of(
+                Arguments.of(1, 1),
+                Arguments.of(2, 11),
+                Arguments.of(1, 10),
+                Arguments.of(15, 78),
+                Arguments.of(15, 690),
+                Arguments.of(7, 700),
+                Arguments.of(21, 777),
+                Arguments.of(1, 1000)
+        );
+    }
+
+    @MethodSource("sumNumbersProviderArguments")
+    @DisplayName("Test for method getSumNumbers() from class Number.")
+    @ParameterizedTest(name = "Sum: {0}")
+    void testSumNumbers(int expected, int value) {
+        var actual = number.getSumNumbers(value);
+
+        assertEquals(expected, actual);
+    }
+
+    static @NotNull Stream<Arguments> multiplicationNumberProviderArguments() {
+        return of(
+                Arguments.of(1, 1),
+                Arguments.of(1, 11),
+                Arguments.of(0, 10),
+                Arguments.of(56, 78),
+                Arguments.of(0, 690),
+                Arguments.of(0, 700),
+                Arguments.of(343, 777),
+                Arguments.of(0, 1000)
+        );
+    }
+
+    @MethodSource("multiplicationNumberProviderArguments")
+    @DisplayName("Test for method getMultiplicationNumber() from class Number.")
+    @ParameterizedTest(name = "Sum: {0}")
+    void testMultiplicationNumber(int expected, int value) {
+        var actual = number.getMultiplicationNumber(value);
+
+        assertEquals(expected, actual);
+    }
 }
